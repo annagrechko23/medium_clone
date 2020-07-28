c35934816af60b63e4bb9650c62094a884a38996
a.hrechko <a.hrechko@mobidev.biz>
2020-07-28 08:23:26 +0300

    pagination

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from 'routes'
-import {  BrowserRouter as Router } from 'react-router-dom'
-import TopBar from 'components/TopBar'
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import TopBar from "components/TopBar";
+import { CurrentUserProvider } from "contexts/currentUser";
+import CurrentUserChecker from "components/currentUserChecker";
+
+
 const App = () => {
   return (
-    <div>
-      <Router>
-        <TopBar />
-        <Routes />
-      </Router>
-    </div>
-  )
-}
+    <CurrentUserProvider>
+      <CurrentUserChecker>
+      <div>
+        <Router>
+          <TopBar />
+          <Routes />
+        </Router>
+      </div>
+      </CurrentUserChecker>
+    </CurrentUserProvider>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
